e5af9423d758240f68eb952ddc83e94e2541d1fd
gentwocoder <adetoyese0511@gmail.com>
2025-10-25 14:01:01 +0100

    update settings

--- hngstage2/hngstage2/settings.py
+++ hngstage2/hngstage2/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x3s)*6lbs6c-16-m^xwhjvhn4k4p)(1n8@m!n2aag%p8zt&n5$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
